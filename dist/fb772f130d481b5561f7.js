f7bee1728b7b7a8d8f59f5cf37d2706d3b0b7429
jkimkim <65081425+jkimkim@users.noreply.github.com>
2022-06-17 18:56:45 +0300

    update

---
+++ dist/fb772f130d481b5561f7.js
@@ -0,0 +1,9 @@
+/* global bootstrap: false */
+(function () {
+  'use strict';
+
+  var tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '../assets/fontawesome/css/fontawesome.css'
 import '../assets/fontawesome/js/fontawesome.js'
+
+import '../css/cover.css'
