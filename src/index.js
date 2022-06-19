78194cd9f919bed1f2655363622167ad27dd695c
jkimkim <65081425+jkimkim@users.noreply.github.com>
2022-06-19 04:16:43 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import 'face-api.js';
+
 import '../assets/fontawesome/css/fontawesome.css'
 import '../assets/fontawesome/js/fontawesome.js'
 
 import '../css/cover.css'
 // import '../js/video'
+
+
